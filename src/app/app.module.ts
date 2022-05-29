8945829c1c1b889791a2343f4dd34463ccf282b1
VishalKhandelwal <vishalshekhar9316@gmail.com>
2022-05-29 18:32:43 +0530

    carousel example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarouselModule } from './carousel/carousel.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
